5705d3e3b2bf2e59e4f6fb303eb78702a58b62e5
Pedro Lemus <pedrolemuz@gmail.com>
2025-03-14 09:23:03 +0100

    Primer Commit

---
+++ DockerGit/src/main/java/com/PracticaDockerYGit/DockerGit/DockerGitApplication.java
@@ -0,0 +1,13 @@
+package com.PracticaDockerYGit.DockerGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerGitApplication.class, args);
+	}
+
+}
